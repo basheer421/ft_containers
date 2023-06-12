b7a5965c7616477b0a5572a98244645a17241698
basheer421 <bammar@student.42abudhabi.ae>
2023-06-12 16:52:35 +0400

    Element access + Capacity done

--- ft/vector.hpp
+++ ft/vector.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:48:05 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/12 00:49:50 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/12 16:51:32 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <memory>
 #include <cstddef>
+#include <stdexcept>
 
 namespace ft
 {
@@ -21,15 +22,8 @@ namespace ft
 template <typename T, typename Alloc = std::allocator<T> >
 class vector
 {
-
-	private:
-		T*			data;
-		std::size_t	_size;
-		std::size_t	_capacity;
-		Alloc allocator;
-
+	// Types
 	public:
-
 		typedef T value_type;
 		typedef Alloc allocator_type;
 
@@ -41,40 +35,152 @@ class vector
 
 		typedef typename allocator_type::size_type size_type;
 		typedef typename allocator_type::difference_type difference_type;
+
+	private:
+		pointer			_data;
+		std::size_t	_size;
+		std::size_t	_capacity;
+		Alloc allocator;
+
+
+		class out_of_range : public std::exception {};
+
+	public:
 		
 		// iterators TODO
 
 		explicit vector (const allocator_type& alloc = allocator_type()):
-			data(NULL),
+			_data(NULL),
 			_size(0),
 			_capacity(0),
 			allocator(alloc)
 		{}
 
 		explicit vector (size_type n, const value_type& val = value_type(),
-			const allocator_type& alloc = allocator_type()); // TODO
+			const allocator_type& alloc = allocator_type())
+			:	_size(n),
+				_capacity(n),
+				allocator(alloc)
+		{
+			_data = allocator.allocate(n);
+			for (size_type i = 0; i < n; i++)
+				allocator.construct(&(_data[i]), val);
+		}
+		
+		vector(const vector& src); // TODO
+
+		vector& operator = (const vector& src); // TODO
+		// TODO
+
 		template <class InputIterator> vector (InputIterator first,
 			InputIterator last, const allocator_type& alloc = allocator_type());
 			// TODO
 
-		~vector(); // TODO
-		vector(const vector& src); // TODO
-		vector& operator = (const vector& src); // TODO
-		// TODO
+		~vector()
+		{
+			if (!_data)
+				return ;
+			for (size_type i = 0; i < _size; i++)
+				allocator.destroy(&(_data[i]));
+			allocator.deallocate(_data, _size);
+		}
 
 
-		/* Capacity: */
+		/* --- Capacity: --- */
 		size_type size() const {return _size;}
+		
 		size_type max_size() const {return allocator.max_size();}
-		// resize TODO
+		
+		void reserve(size_type n)
+		{
+			pointer tmp;
+			size_type i;
+
+			if (n <= _capacity)
+				return ;
+			tmp = _data;
+			_data = allocator.allocate(n);	
+			for (i = 0; i < _size; i++)
+				_data[i] = tmp[i];
+			allocator.deallocate(tmp, _capacity);
+			_capacity = n;
+		}
+
+		void resize(size_type n, value_type val = value_type())
+		{
+			if (n == _size)
+				return ;
+			if (n < _size)
+			{
+				for (size_type i = n - 1; i < _size; i++)
+					allocator.destroy(&(_data[i]));
+				_size = n;
+				return ;
+			}
+			reserve(n);
+			for (size_type i = _size; i < n; i++)
+				allocator.construct(&(_data[i]), val);
+			_size = n;
+		}
+
 		size_type capacity() const {return _capacity;}
-		bool empty() const {return (size == 0);}
-		// reserve TODO
-		// shrink_to_fit TODO
 
+		bool empty() const {return (_size == 0);}
 
-	
+		/* --- Element access --- */
+		reference operator[](size_type n)
+		{
+			return (_data[n]);
+		}
 
+		const_reference operator[](size_type n) const
+		{
+			return (_data[n]);
+		}
+
+		reference at(size_type n)
+		{
+			if (n >= size())
+				throw out_of_range();
+			return (_data[n]);
+		}
+
+		const_reference at(size_type n) const
+		{
+			if (n >= size())
+				throw out_of_range();
+			return (_data[n]);
+		}
+
+		reference front()
+		{
+			return (_data[0]);
+		}
+
+		const_reference front() const
+		{
+			return (_data[0]);
+		}
+
+		reference back()
+		{
+			return (_data[_size - 1]);
+		}
+
+		const_reference back() const
+		{
+			return (_data[_size - 1]);
+		}
+
+		value_type* data()
+		{
+			return (_data);
+		}
+
+		const value_type* data() const
+		{
+			return (_data);
+		}
 };
 
 }
--- main.cpp
+++ main.cpp
@@ -3,17 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:45:09 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/12 00:45:45 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/12 16:51:44 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
 #include "ft/vector.hpp"
+#include <vector>
 
 int main(void)
 {
+	ft::vector<std::string> f(3);
+	std::vector<std::string> s(3);
+	
+	for (int i = 0; i < 3; i++)
+		f[i] = "hi";
+	for (int i = 0; i < 3; i++)
+		s[i] = "hi";
+	
+	f.resize(2, "k");
+	s.resize(2, "k");
+
+	for (size_t i = 0; i < f.size(); i++)
+		std::cout << f[i] << "\n";
+
+	for (size_t i = 0; i < s.size(); i++)
+		std::cout << s[i] << "\n";
+
+	std::cout << f.capacity() << "\n";
+	std::cout << s.capacity() << "\n";
+
+
 	
 	return (0);
 }
