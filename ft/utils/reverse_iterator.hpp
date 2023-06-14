3d407b4087f55d22ecf5ebac34bda979cadea985
basheer laptop <basheer753@gmail.com>
2023-06-15 01:56:37 +0400

    reverse_iterator

--- ft/utils/reverse_iterator.hpp
+++ ft/utils/reverse_iterator.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   reverse_iterator.hpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 19:10:36 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/14 23:33:53 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/15 01:55:19 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,140 +17,134 @@
 namespace ft
 {
 
-template <class It>
+template <class Iter>
 class reverse_RAI_iterator
 {
-    // Types
     public:
-        typedef It                              value_type;
-        typedef It*                             pointer;
-        typedef It&                             reference;
-        typedef ptrdiff_t                      difference_type;
-        typedef random_access_iterator_tag     iterator_category;
+        typedef Iter                                        iterator_type;
+        typedef typename iterator_type::value_type          value_type;
+        typedef typename iterator_type::pointer             pointer;
+        typedef typename iterator_type::reference           reference;
+        typedef typename iterator_type::difference_type     difference_type;
+        typedef typename iterator_type::iterator_category   iterator_category;
     
     private:
-        RAI_iterator<It> normal_it;
-    
+        iterator_type current;
+
     public:
-        reverse_RAI_iterator()
-            :   normal_it(NULL)
-        {}
+        reverse_RAI_iterator():current(NULL) {}
 
-        reverse_RAI_iterator(It value)
-            :   normal_it(value)
-        {}
-    
-        // reverse_RAI_iterator(const RAI_iterator<It> normal_iterator)
-        //     :   normal_it(normal_iterator)
-        // {}
+        explicit reverse_RAI_iterator(iterator_type x):current(x) {}
 
-        ~reverse_RAI_iterator() {}
-
-        reverse_RAI_iterator(const reverse_RAI_iterator& src)
+        template <class U>
+        reverse_RAI_iterator(const reverse_RAI_iterator<U>& src)
         {
             if (this == &src)
                 return ;
             *this = src;
         }
 
-        reverse_RAI_iterator& operator=(const reverse_RAI_iterator& src)
+        template <class U>
+        reverse_RAI_iterator& operator=(const reverse_RAI_iterator<U>& src)
         {
             if (this == &src)
                 return *this;
-            normal_it = src.normal_it;
+            current = src.current;
             return *this;
         }
 
-        RAI_iterator base() const {return normal_it;}
+        iterator_type base() const {return current;}
 
-        reference operator*() const {return *normal_it;}
+        reference operator*() const {return *current;}
 
         pointer operator->() const {return &(operator*());}
 
-        reverse_RAI_iterator& operator++() {--normal_it; return *this;}
+        reference operator[](difference_type n) const {return *(current - n);}
+
+        reverse_RAI_iterator& operator++() {--current; return *this;}
 
         reverse_RAI_iterator operator++(int)
+        {
+            reverse_RAI_iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+        
+        reverse_RAI_iterator& operator--() {++current; return *this;}
+
+        reverse_RAI_iterator operator--(int)
         {
             reverse_RAI_iterator tmp = *this;
             --(*this);
             return tmp;
         }
 
-        reverse_RAI_iterator& operator--() {++normal_it; return *this;}
-
-        reverse_RAI_iterator operator--(int)
-        {
-            reverse_RAI_iterator tmp = *this;
-            ++(*this);
-            return tmp;
-        }
-
         reverse_RAI_iterator operator+(difference_type n) const
         {
-            return reverse_RAI_iterator<It>(normal_it - n);
-        }
-        
-        reverse_RAI_iterator operator-(difference_type n) const
-        {
-            return reverse_RAI_iterator<It>(normal_it + n);
+            return reverse_RAI_iterator((current - n));
         }
 
-        template <class T2>
-        reverse_RAI_iterator operator-(const reverse_RAI_iterator<T2>& b) const
+        reverse_RAI_iterator operator-(difference_type n) const
         {
-            return reverse_RAI_iterator<It>(normal_it + b);
+            return reverse_RAI_iterator((current + n));
         }
 
         reverse_RAI_iterator& operator+=(difference_type n)
         {
-            normal_it -= n;
-            return (*this);
+            current -= n;
+            return *this;
         }
 
         reverse_RAI_iterator& operator-=(difference_type n)
         {
-            normal_it += n;
-            return (*this);
+            current += n;
+            return *this;
         }
 
-        reference operator[](difference_type n) {return *(normal_it - n);}
-
-        template <class T2>
-        bool operator==(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it == b.base();
-        }
-
-        template <class T2>
-        bool operator!=(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it != b.base();
-        }
-
-        template <class T2>
-        bool operator<(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it > b.base();
-        }
-
-        template <class T2>
-        bool operator<=(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it >= b.base();
-        }
-
-        template <class T2>
-        bool operator>(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it < b.base();
-        }
-
-        template <class T2>
-        bool operator>=(const reverse_RAI_iterator<T2>& b) const
-        {
-            return normal_it <= b.base();
-        }
 };
+
+template< class Iterator1, class Iterator2 >
+bool operator==( const reverse_RAI_iterator<Iterator1>& lhs,
+                 const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs.base() == rhs.base());
+}
+
+template< class Iterator1, class Iterator2 >
+bool operator!=( const reverse_RAI_iterator<Iterator1>& lhs,
+                 const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs.base() != rhs.base());
+}
+
+template< class Iterator1, class Iterator2 >
+bool operator<( const reverse_RAI_iterator<Iterator1>& lhs,
+                const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs > rhs);
+}
+
+template< class Iterator1, class Iterator2 >
+bool operator<=( const reverse_RAI_iterator<Iterator1>& lhs,
+                 const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs.base() >= rhs.base());
+}
+
+template< class Iterator1, class Iterator2 >
+bool operator>( const reverse_RAI_iterator<Iterator1>& lhs,
+                const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs.base() < rhs.base());
+}
+
+template< class Iterator1, class Iterator2 >
+bool operator>=( const reverse_RAI_iterator<Iterator1>& lhs,
+                const reverse_RAI_iterator<Iterator2>& rhs )
+{
+    return (lhs.base() <= rhs.base());
+}
+
 
 // "n + a" case
 template <class It>
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:45:09 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/14 23:25:32 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/15 01:55:54 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ int main(void)
 		stl.push_back(i);
 	}
 
-	for (ft::vector<int>::reverse_iterator it = mine.rbegin(); it != mine.rend(); it++)
+	for (ft::vector<int>::reverse_iterator it = mine.rbegin() + 1; it != mine.rend(); it++)
 	{
 		std::cout << *it << "\n";
 	}
 	
 	std::cout << "------\n";
 
-	for (std::vector<int>::reverse_iterator it = stl.rbegin(); it != stl.rend(); it++)
+	for (std::vector<int>::reverse_iterator it = stl.rbegin() + 1; it != stl.rend(); it++)
 	{
 		std::cout << *it << "\n";
 	}
