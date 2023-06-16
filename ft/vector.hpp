a4afefab4bfd0fbcd9925576f8bbf59797c260df
basheer laptop <basheer753@gmail.com>
2023-06-16 23:32:41 +0400

    ft:swap added

--- ft/vector.hpp
+++ ft/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:48:05 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/16 23:18:14 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/16 23:23:25 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -463,6 +463,13 @@ bool operator>=( const ft::vector<T, Alloc>& lhs,
                  const ft::vector<T, Alloc>& rhs )
 {
 	return (!(lhs < rhs));
+}
+
+template< class T, class Alloc >
+void swap( ft::vector<T, Alloc>& lhs,
+           ft::vector<T, Alloc>& rhs )
+{
+	lhs.swap(rhs);
 }
 
 }
