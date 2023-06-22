ef43016134ec396ba37e58c9981d4591ff693f11
basheer laptop <basheer753@gmail.com>
2023-06-23 01:52:54 +0400

    ready to add more tests

--- my_tester/tests/vector/constructor.cpp
+++ my_tester/tests/vector/constructor.cpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 20:49:56 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/23 01:20:52 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/23 01:52:18 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,23 @@
 
 void default_constructor()
 {
-    NAMESPACE::vector<int> x;
+    NAMESPACE::vector<int> x1;
+    NAMESPACE::vector<std::string> x2;
     NAMESPACE::vector<int>* new_vector = new NAMESPACE::vector<int>();
+    NAMESPACE::vector<std::string>* new_vector2 = new NAMESPACE::vector<std::string>();
+    
+    print_vector(x1);
+    print_vector(x2);
+    print_vector(*new_vector);
+    print_vector(*new_vector2);
+
     delete new_vector;
-    print_vector(x);
+    delete new_vector2;
+    
 }
 
+void alloc_constructor()
+{
+}
 
 #endif
