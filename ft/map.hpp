f02ab394ae66a726b9ea24bfe2ba0fe0550fa18f
basheer laptop <basheer753@gmail.com>
2023-06-23 21:53:51 +0400

    compiling

--- ft/map.hpp
+++ ft/map.hpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 15:34:56 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/23 17:04:26 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/23 21:51:59 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ namespace ft
     class Key,
     class T,
     class Compare = std::less<Key>,
-    class Allocator = std::allocator<ft::pair<const Key, T>>
+    class Allocator = std::allocator<ft::pair<const Key, T> >
 > class map
 {
 	// Types
@@ -37,17 +37,17 @@ namespace ft
 		typedef Key key_type;
 		typedef T mapped_type;
 		typedef ft::pair<const Key, T> value_type;
+		typedef Allocator allocator_type;
 		typedef typename allocator_type::size_type size_type;
 		typedef typename allocator_type::difference_type difference_type; 
 		typedef Compare key_compare;
-		typedef Allocator allocator_type;
 		typedef value_type& reference;
 		typedef const value_type& const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
 
 	private:
-		ft::rb_tree<key_type, mapped_type, key_compare, allocator_type> tree;
+		ft::rb_tree<key_type, mapped_type> tree;
 		size_type _size;
 		allocator_type allocator;
 		key_compare compare;
@@ -55,15 +55,13 @@ namespace ft
 	public:
 		explicit map(const key_compare& comp = key_compare(),
 			const allocator_type& alloc = allocator_type())
-			:	tree(alloc),
-				_size(0),
+			:	_size(0),
 				allocator(alloc),
 				compare(comp)
 		{}
 
 		map(const map& x)
-		:	tree(x.allocator),
-			_size(0),
+		:	_size(0),
 			allocator(x.allocator),
 			compare(x.compare)
 		{
@@ -88,6 +86,21 @@ namespace ft
 
 		~map() {}
 
+		T& at(const Key& key)
+		{
+			return tree.get(key);
+		}
+
+		T& operator[](const Key& key)
+		{
+			try {
+				return (at(key));
+			} catch (std::out_of_range& e) {
+				tree.insert(ft::make_pair(key, T()));
+			}
+			return at(key);
+		}
+
 		bool empty() const
 		{
 			return (tree.get_root() == NULL);
@@ -98,34 +111,24 @@ namespace ft
 			return (_size);
 		}
 
+		allocator_type get_allocator() const {return allocator;}
+
+		size_type max_size() const
+		{
+			return (tree.get_allocator().max_size());
+		}
+
+		// std::pair<iterator, bool> insert(const value_type& value)
+		// {
+		// 	tree.insert(value);
+		// }
+
 		void clear()
 		{
 			~tree();
 			_size = 0;
 		}
 
-		allocator_type get_allocator() const {return allocator;}
-
-		T& at(const Key& key)
-		{
-			tree.get(key);
-		}
-
-		// T& operator[](const Key& key)
-		// {
-		// 	try {
-		// 		return (tree.get(key));
-		// 	} catch (std::out_of_range& e) {
-		// 		(void) // for now
-		// 	}
-		// }
-
-		size_type max_size() const
-		{
-			// return (tree.get_allocator().max_size());
-			return (120); // 256(stack size) / 2 = 128 and -8 for safety.
-		}
-
-}
+};
 
 }
--- main.cpp
+++ main.cpp
@@ -6,48 +6,21 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:45:09 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/23 15:18:02 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/23 21:53:06 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
-#include "ft/vector.hpp"
-#include <vector>
-#include <stack>
-#include "ft/stack.hpp"
-#include "ft/utils/pair.hpp"
-#include "ft/utils/rb_tree.hpp"
-
-
-void print_container(ft::vector<ft::pair<int, double> > c) 
-{
-    for (ft::vector<ft::pair<int, double> >::iterator it = c.begin(); it != c.end(); it++)
-    {
-      std::cout << (*it).first << ", " << (*it).second;
-      std::cout << '\n';
-    }
-}
-
+// #include "ft/map.hpp"
+// #include <map>
+#include "ft/map.hpp"
 
 int main ()
 {
-  ft::rb_tree<std::string, std::string> tree;
-
-  // tree.insert(ft::make_pair("1", "wow"));
-  tree.insert(ft::make_pair("-2", "ok"));
-  tree.insert(ft::make_pair("3", "90"));
-  tree.insert(ft::make_pair("4", "120"));
-  tree.insert(ft::make_pair("550", "0"));
-  tree.insert(ft::make_pair("4", "110"));
-
-  tree.remove("-2");
-  tree.remove("3");
-  std::cout << "Inorder traversal of the LLRB tree: \n";
-  tree.printInorder();
-  std::cout << std::endl;
-
-  std::cout << tree.get_root()->pr.first << "\n";
+  ft::map<std::string, int>x;
+  x["x"] = 1;
+  std::cout << x.at("x") << "\n";
 
   return 0;
 }
